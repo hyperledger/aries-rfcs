04c3a5d3156d2829cf1b57f4d298d52a8c74eaa3
Daniel Hardman <daniel.hardman@gmail.com>
2019-06-14 16:57:05 -0600

    Make test pass
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- code/test_index.py
+++ code/test_index.py
@@ -3,20 +3,17 @@ import pytest
 import tempfile
 
 @pytest.fixture
-def code_cwd():
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-@pytest.fixture
 def scratch_space():
     x = tempfile.TemporaryDirectory()
     yield x
     x.cleanup()
 
-def test_index(code_cwd, scratch_space):
+def test_index(scratch_space):
     temp_idx = os.path.join(scratch_space.name, 'index.md')
     import generate_index
     generate_index.main(temp_idx)
-    if os.system('diff ../index.md ' + temp_idx):
+    perm_idx = os.path.join(os.path.dirname(__file__), '../index.md')
+    if os.system('diff %s %s' % (perm_idx, temp_idx)):
         pytest.fail("/index.md needs to be updated. Run python code/generate_index.py.")
     
 
